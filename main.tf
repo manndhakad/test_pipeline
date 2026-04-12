badd764d35bc32c7927e2284fcfeacbbc835ca81
manndhakad <mdhakad1189@gmail.com>
2026-04-12 16:06:06 +0530

    storage account name changed

--- main.tf
+++ main.tf
@@ -20,13 +20,13 @@ resource "azurerm_resource_group" "rg_pipeline02" {
 }
 
 resource "azurerm_resource_group" "rg_pipeline" {
-    name     = "sanjay-rg-pipeline02"
+    name     = "sanjay-rg-pipeline022"
     location = "central india"
   
 }
 
 resource "azurerm_storage_account" "st_pipeline" {
-    name                    = "stpipeline1123"
+    name                    = "stpipeline11223"
     resource_group_name      = azurerm_resource_group.rg_pipeline.name
     location                 = azurerm_resource_group.rg_pipeline.location
     account_tier             = "Standard"
