a781c852848bf3a5e5252432da6b67d3b97aa253
manndhakad <mdhakad1189@gmail.com>
2026-04-12 15:06:52 +0530

    terraform code

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+  
+}
+
+resource "azurerm_resource_group" "rg_pipeline02" {
+    name     = "rg-pipeline02"
+    location = "central india"
+  
+}
+
+resource "azurerm_resource_group" "rg_pipeline" {
+    name     = "sanjay-rg-pipeline02"
+    location = "central india"
+  
+}
+
+resource "azurerm_storage_account" "st_pipeline" {
+    name                    = "stpipeline1123"
+    resource_group_name      = azurerm_resource_group.rg_pipeline.name
+    location                 = azurerm_resource_group.rg_pipeline.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+  
+}
